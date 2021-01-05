91f556511db19e4a442902bcb6a8178985686e9a
salzinger <salzinger@physi.uni-heidelberg.de>
2021-01-05 11:20:09 +0100

    hello darkness my old friend

--- BetAndWin.py
+++ BetAndWin.py
@@ -21,64 +21,70 @@ def put_value(underlying, put_strike_price, put_break_even, put_initial_invest, 
 
 t = np.linspace(0, 14, 8)
 
-#SP500
-current_underlying = 3450
+#EURO Stoxx 50
+current_underlying = 3540
 #https://wertpapiere.ing.de/Investieren/Derivat/DE000VP7XK01
-put_strike_price = 3700
-put_break_even = 3433.64
-put_initial_invest = 4000
-put_premium_percentage = 0.5/100
+put_strike_price = 4200
+put_break_even = 3157
+put_initial_invest = 3000
+put_premium_percentage = 11.5/100 *0
 #https://wertpapiere.ing.de/Investieren/Derivat/CH0540336895
-call_strike_price = 2660
-call_break_even = 3448.38
-call_initial_invest = 10000-put_initial_invest
-call_premium_percentage = 0.1/100
-#https://wertpapiere.ing.de/Investieren/Derivat/DE000VP6TG79
-call_strike_price = 3600
-call_break_even = 3633
-call_initial_invest = 10000-put_initial_invest
-call_premium_percentage = 5.2/100
-#https://wertpapiere.ing.de/Investieren/Derivat/DE000VP6TG38
-call_strike_price = 3450
-call_break_even = 3552.537
-call_initial_invest = 10000-put_initial_invest
-call_premium_percentage = 2.9/100
-#https://wertpapiere.ing.de/Investieren/Derivat/DE000VP6TGV5
-call_strike_price = 3100
-call_break_even = 3475.9
-call_initial_invest = 10000-put_initial_invest
-call_premium_percentage = 0.7/100
-#https://wertpapiere.ing.de/Investieren/Derivat/CH0456737656
-call_strike_price = 2400
-call_break_even = 3447.96
-call_initial_invest = 10000-put_initial_invest
-call_premium_percentage = -0.13/100
-#https://wertpapiere.ing.de/Investieren/Derivat/CH0540336952
-call_strike_price = 2780
-call_break_even = 3451.92
-call_initial_invest = 8000
-call_premium_percentage = 0.1/100
+call_strike_price = 2200
+call_break_even = 3421
+call_initial_invest = 7000-put_initial_invest
+call_premium_percentage = -4/100 *0
 
 
 
-underlying = np.linspace(put_break_even * 0.4, call_break_even * 1.6, 100)
+#NVIDIA
+current_underlying = 533.3
+#https://wertpapiere.ing.de/Investieren/Derivat/DE000HZ92279
+put_strike_price = 200
+put_break_even = 199.98
+put_initial_invest = 1
+put_premium_percentage = 62/100
+#https://wertpapiere.ing.de/Investieren/Derivat/DE000HZ92279
+call_strike_price = 275
+call_break_even = 536.5
+call_initial_invest = 4000-put_initial_invest
+call_premium_percentage = 0.8/100
 
-combined = call_value(underlying, call_strike_price, call_break_even, call_initial_invest, call_premium_percentage) + put_value(underlying, put_strike_price, put_break_even, put_initial_invest, put_premium_percentage)
+
+#Alibaba
+current_underlying = 227.85
+#https://wertpapiere.ing.de/Investieren/Derivat/DE000JC57SA4
+put_strike_price = 298
+put_break_even = 231
+put_initial_invest = 500
+put_premium_percentage = 0.5/100
+
+#https://finance.yahoo.com/quote/BABA/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuc3RhcnRwYWdlLmNvbS8&guce_referrer_sig=AQAAAJxltTxoz6hR3t-DqbMrBx7jfhWa5Q5z3TEGKqGUvOn0szniJ1CiQn1OGR_xwQwUrJm7eeOmLtY0tplGpmYs_fJBlsHAVYLM0oepAPMYkTsY3EWfCo-KUJRfQUaiMnKuFUkMKSAuebS9mXp0_dar6mPjwpesnRwuvGAy85OuJ0cs
+
+stock_initial_invest = 2000-put_initial_invest
+
+
+
+
+
+
+underlying = np.linspace(put_break_even * 0, call_break_even * 2, 100)
+
+combined = call_value(underlying, call_strike_price, call_break_even, call_initial_invest, call_premium_percentage) + put_value(underlying, put_strike_price, put_break_even, put_initial_invest, put_premium_percentage) + (underlying/current_underlying-1)*stock_initial_invest
 
 double_bagger = combined - call_initial_invest * (1 + call_premium_percentage) - put_initial_invest * (1 + put_premium_percentage)
 
 zero_crossings = np.where(np.diff(np.sign(combined)))[0]
 bagger_crossings = np.where(np.diff(np.sign(double_bagger)))[0]
 loss = 0
-for loss_values in range(zero_crossings[0], zero_crossings[1]):
-    loss += underlying[loss_values]
+#for loss_values in range(zero_crossings[0], zero_crossings[1]):
+#    loss += underlying[loss_values]
 
-print('Loss Area: ', loss/(call_initial_invest * (1 + call_premium_percentage) + put_initial_invest * (1 + put_premium_percentage)))
-print("Double for %.2f: " % underlying[bagger_crossings[0]], "at %.2f percent" % ((underlying[bagger_crossings[0]]/current_underlying-1)*100) )
-print("Break for %.2f: " % underlying[zero_crossings[0]], "at %.2f percent" % ((underlying[zero_crossings[0]]/current_underlying-1)*100) )
-print("Max Loss: %.2f" % np.min(combined))
-print("Break for %.2f: " % underlying[zero_crossings[1]], "at %.2f percent" % ((underlying[zero_crossings[1]]/current_underlying-1)*100) )
-print("Double for %.2f: " % underlying[bagger_crossings[1]], "at %.2f percent" % ((underlying[bagger_crossings[1]]/current_underlying-1)*100) )
+#print('Loss Area: ', loss/(call_initial_invest * (1 + call_premium_percentage) + put_initial_invest * (1 + put_premium_percentage)))
+#print("Double for %.2f: " % underlying[bagger_crossings[0]], "at %.2f percent" % ((underlying[bagger_crossings[0]]/current_underlying-1)*100) )
+#print("Break for %.2f: " % underlying[zero_crossings[0]], "at %.2f percent" % ((underlying[zero_crossings[0]]/current_underlying-1)*100) )
+#print("Max Loss: %.2f" % np.min(combined))
+#print("Break for %.2f: " % underlying[zero_crossings[1]], "at %.2f percent" % ((underlying[zero_crossings[1]]/current_underlying-1)*100) )
+#print("Double for %.2f: " % underlying[bagger_crossings[1]], "at %.2f percent" % ((underlying[bagger_crossings[1]]/current_underlying-1)*100) )
 
 fig, ax = plt.subplots()
 
@@ -86,6 +92,8 @@ ax.plot(underlying, call_value(underlying, call_strike_price, call_break_even, c
 ax.plot(underlying, put_value(underlying, put_strike_price, put_break_even, put_initial_invest, put_premium_percentage), label="Put", marker='', linestyle='-', markersize='2')
 ax.plot(underlying, combined, label="Combined", marker='', linestyle='-', markersize='2')
 ax.plot(underlying, np.zeros(underlying.shape) , marker='', linestyle='-', markersize='2')
+ax.plot(underlying, (underlying/current_underlying-1)*stock_initial_invest, label="Stocks", marker='', linestyle='-', markersize='2')
+
 # ax.plot(times, result.expect[1],label="MagnetizationZ",linestyle='--',marker='o',markersize='2');
 # ax.plot(times, result.expect[2],label="Exp(SigmaZ,0)");
 # ax.plot(times, result.expect[3],label="Exp(SigmaX,0)",linestyle='--');
